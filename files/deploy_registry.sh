cdbcc4ad2764c3a8c1c9dd3fa58824b7cc141fb0
Kay Hannay <klinux@hannay.de>
2020-11-16 22:11:20 +0100

    feat: install release tool

--- files/deploy_registry.sh
+++ files/deploy_registry.sh
@@ -1,43 +1,55 @@
 #!/usr/bin/env bash
 
-PACKAGE="${1}"
-SOFTWARE="${2}"
-VERSION="${3}"
-FTP_USER="${4}"
-FTP_SECRET="${5}"
+FTP_USER="${1}"
+FTP_SECRET="${2}"
+DISTRIBUTION="${3}"
+SOFTWARE="${4}"
+VERSION="${5}"
+CREATE_RELEASE=true
 
 set -e
 
-if [ -z "${PACKAGE}" ]; then
-    echo "First parameter PACKAGE is missing!"
-    exit 1
+if [ -f release_info.sh ]; then
+    source release_info.sh
 fi
 
-if [ -z "${SOFTWARE}" ]; then
-    echo "Second parameter SOFTWARE is missing!"
-    exit 1
-fi
-
-if [ -z "${VERSION}" ]; then
-    echo "Third parameter VERSION is missing!"
-    exit 1
+if [ "${CREATE_RELEASE}" != "true" ]; then
+    echo "Skip deployment since there is no new release"
+    exit 0
 fi
 
 if [ -z "${FTP_USER}" ]; then
-    echo "Fourth parameter FTP_USER is missing!"
+    echo "First parameter FTP_USER is missing!"
     exit 1
 fi
 
 if [ -z "${FTP_SECRET}" ]; then
-    echo "Fifth parameter FTP_SECRET is missing!"
+    echo "Second parameter FTP_SECRET is missing!"
     exit 1
 fi
+
+if [ -z "${DISTRIBUTION}" ]; then
+    echo "Third parameter DISTRIBUTION is missing!"
+    exit 1
+fi
+
+if [ -z "${SOFTWARE}" ]; then
+    echo "Fourth parameter SOFTWARE is missing!"
+    exit 1
+fi
+
+if [ -z "${VERSION}" ]; then
+    echo "Fifth parameter VERSION is missing!"
+    exit 1
+fi
+
+PACKAGE="${SOFTWARE}_${VERSION}_all.deb"
 
 lftp -e "mirror -n debian repository && bye" --user "${FTP_USER}" --password "${FTP_SECRET}" ftp://wp1152936.server-he.de
 
 cd repository
-reprepro remove $VERSION $SOFTWARE
-reprepro -Vb . includedeb $VERSION ../$PACKAGE
+reprepro remove $DISTRIBUTION $SOFTWARE
+reprepro -Vb . includedeb $DISTRIBUTION ../$PACKAGE
 cd ..
 
 lftp -e "mirror -R -n repository debian && bye" -u "${FTP_USER}","${FTP_SECRET}" ftp://wp1152936.server-he.de
